e31b924921f32c538212d9a53b8ac178d28382d2
Arshia Akhavan <letmemakenewone@gmail.com>
2020-09-25 11:34:19 +0330

    roft o rub

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,7 @@
 import model.ConfigMap;
-import model.probe.ProbeManager;
 
 public class Main {
     public static void main(String[] args) {
-        ProbeManager probeManager = new ConfigMap().load();//.init();
-        probeManager.init();
-        ProbeManager ne =new ProbeManager(7000);
-        ne.init();
+        new ConfigMap().load().init();
     }
 }
--- src/main/java/model/probe/ProbeManager.java
+++ src/main/java/model/probe/ProbeManager.java
@@ -28,7 +28,6 @@ public class ProbeManager {
             System.out.println();
             server.addContext(r.getPath(), t -> {
                 r.probes.get(0).handle(new String(t.getRequestBody().readAllBytes(), "UTF-8"));
-//                r.handle(new String(t.getRequestBody().readAllBytes(), "UTF-8"));
                 t.sendResponseHeaders(200, 0);
                 t.getResponseBody().write("".getBytes());
                 t.getResponseBody().close();
